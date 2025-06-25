bcb53ec26f7f8e119486d0520d0323e7ef2059bc
Juan David <juan.ramirez.freire@udla.edu.ec>
2025-06-25 17:47:16 -0500

    Changes

--- app.py
+++ app.py
@@ -3,26 +3,21 @@ import pickle
 import os
 
 def unsafe_function(user_input):
-    # Esto es inseguro y Bandit lo detectará (Shell Injection)
     subprocess.call("echo " + user_input, shell=True)
 
 def insecure_pickle_load(filename):
-    # Carga insegura de archivos pickle (vulnerable a ejecución de código arbitrario)
     with open(filename, "rb") as f:
         data = pickle.load(f)
     return data
 
 def hardcoded_password():
-    # Contraseña en texto plano (Bandit la detecta)
     password = "SuperSecret123"
     print("La contraseña es:", password)
 
 def use_eval(data):
-    # Uso peligroso de eval() (vulnerabilidad crítica)
     print(eval(data))
 
 def temp_file_insecure():
-    # Creación insegura de archivos temporales (posible ataque de carrera)
     temp_filename = "/tmp/mi_archivo_temp.txt"
     with open(temp_filename, "w") as temp_file:
         temp_file.write("Archivo temporal inseguro")
